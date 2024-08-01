ee0886e732535e0a4f7596d265102652ac714cd3
Sreeharsha Raveendra <dev.sreeharsha@gmail.com>
2024-08-01 23:59:39 +0000

    feat: cognito form demo component

--- cognito-forms-demo/src/app/app.routes.ts
+++ cognito-forms-demo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CognitoFormComponent } from './cognito-form/cognito-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CognitoFormComponent}
+];
